b6d3c6d1323c750b3c5b18c5e787cb594e74619f
mgarcia95 <manolo95gl@gmail.com>
2022-11-05 14:39:53 +0000

    Jenkinsfile dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'gtpa6z',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
