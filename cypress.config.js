e58ac92ebbf734bd3957681020661d2659fa77c3
mgarcia95 <manolo95gl@gmail.com>
2022-11-07 16:29:15 +0000

    Jenkinsfile reports fix2

--- cypress.config.js
+++ cypress.config.js
@@ -6,14 +6,14 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    
   },
-  {
-    "reporter": "mochawesome",
-    "reporterOptions": {
-      "reportDir": "cypress/reports/mochawesome-report",
-      "overwrite": false,
-      "html": false,
-      "json": true,
-      "timestamp": "mmddyyyy_HHMMss"
-    },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports/mochawesome-report",
+      overwrite: false,
+      html: false,
+      json: true,
+      timestamp: "mmddyyyy_HHMMss"
+    }
 });
